9092739f7318be0aa1823d01707eefc5dbe8017d
Mattia Verga <mattia.verga@tiscali.it>
2019-12-21 18:18:57 +0100

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('./pyongc/')
+sys.path.append(os.path.abspath('../pyongc/'))
 
 
 # -- Project information -----------------------------------------------------
