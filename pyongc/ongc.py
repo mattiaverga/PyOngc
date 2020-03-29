5c5ae3e957c9b60baca796b644176f97f688b040
Mattia Verga <mattia.verga@tiscali.it>
2020-03-29 11:52:00 +0200

    Bump version to 0.6.0

--- pyongc/ongc.py
+++ pyongc/ongc.py
@@ -46,7 +46,7 @@ import sqlite3
 
 from pyongc import InvalidCoordinates, ObjectNotFound, UnknownIdentifier
 
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 DBDATE = 20200329  # Version of database data
 DBPATH = resource_filename(__name__, 'ongc.db')
 PATTERNS = {'NGC|IC': r'^((?:NGC|IC)\s?)(\d{1,4})\s?((NED)(\d{1,2})|[A-Z]{1,2})?$',
