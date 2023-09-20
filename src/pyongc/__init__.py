d2c287167a5a16b7d23cbca161c2d2c426849c69
Mattia Verga <mattia.verga@tiscali.it>
2023-09-20 07:59:11 +0200

    Update version to 1.0.2
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

--- src/pyongc/__init__.py
+++ src/pyongc/__init__.py
@@ -4,10 +4,11 @@
 
 """Python interface to OpenNGC database data."""
 
+from importlib.metadata import version
 from pkg_resources import resource_filename
 
 
-__version__ = '1.0.1'
+__version__ = version('PyOngc')
 DBDATE = 20221023  # Version of database data
 
 DBPATH = resource_filename(__name__, 'ongc.db')
