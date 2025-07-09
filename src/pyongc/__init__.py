a5fdad9b42c419932cf71d11c3a7e5facf362819
Mattia Verga <mattia.verga@tiscali.it>
2025-07-09 17:52:49 +0200

    Release 1.2.0
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

--- src/pyongc/__init__.py
+++ src/pyongc/__init__.py
@@ -9,6 +9,6 @@ from importlib.resources import files
 
 
 __version__ = version('PyOngc')
-DBDATE = 20221023  # Version of database data
+DBDATE = 20231203  # Version of database data
 
 DBPATH = str(files(__name__) / 'ongc.db')
